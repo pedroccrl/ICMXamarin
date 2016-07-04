94c152421ec1cc42896c2d2dd97cae6c98f5c984
vnoan <Victor@VICTOR-PC>
2016-07-04 15:27:13 -0300

    qualquer coisa

---
+++ ICMXamarin/ICMXamarin/ViewModel/FotoPageVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ICMXamarin.ViewModel
+{
+	public class FotoPageVM : INotifyPropertyChanged
+	{
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public FotoPageVM ()
+		{
+            
+		}
+
+        private void TakeFoto()
+        {
+
+        }
+
+    }
+}
